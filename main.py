25d8b36464d7e1b06d92514252f021903f8c4da5
Jessica Guio <jessicaguio110595@gmail.com>
2022-10-29 13:20:08 -0400

    First flask app version with root endpoint for testing purposes

--- main.py
+++ main.py
@@ -7,5 +7,26 @@ from flask_cors import CORS
 from waitress import serve
 
 
+app = Flask(__name__)
+cors = CORS(app)
+
+
+@app.route("/", methods=['GET'])
+def home():
+    json_ = {"message": "Welcome to the academic microservices..."}
+    return json_
+
+
+# Config and execute app
+
+def load_file_config():
+    with open("config.json") as file:
+        data = json.load(file)
+    return data
+
+
 if __name__ == '__main__':
-    print("Hola G17")
+    data_config = load_file_config()
+    print("Server running: http://"+data_config.get('url-backend')+":"+str(data_config.get('port')))
+    serve(app, host=data_config.get('url-backend'), port=data_config.get('port'))
+
