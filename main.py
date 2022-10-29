c221f55eda8429cff1ea8078ee533124257a731d
Jessica Guio <jessicaguio110595@gmail.com>
2022-10-29 12:52:54 -0400

    initial project commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import json
+
+from flask import Flask
+from flask import jsonify
+from flask import request
+from flask_cors import CORS
+from waitress import serve
+
+
+if __name__ == '__main__':
+    print("Hola G17")
